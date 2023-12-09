c2f4bc33a99226bc3b04b8536e8484db482f1843
Herman <palchukgerman@gmail.com>
2023-12-10 01:23:26 +0300

    new commit

--- dy_second_page.py
+++ dy_second_page.py
@@ -1,10 +1,9 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import PhotoImage
-from sympy import sympify, Symbol
-import numpy as np
 import time
 import matplotlib.pyplot as plt
+
 
 class DySecondPage(tk.Frame):
     def __init__(self, parent, controller):
@@ -149,7 +148,6 @@ class DySecondPage(tk.Frame):
         plt.legend()
 
         plt.pause(0.01)
-
 
     def show_dy_page(self):
         plt.close()
